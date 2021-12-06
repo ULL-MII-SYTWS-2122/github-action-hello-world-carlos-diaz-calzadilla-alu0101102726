fd05d11b80e9742dc11eb7aa947a0707f554bb37
alu0101102726 <alu0101102726@ull.edu.es>
2021-12-06 19:20:48 +0000

    Añadidos templates y template modulo funcional

---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const axios = require("axios");
+const shell = require("shelljs");
+
+let templateFiles = 'templates/'
+
+const category = core.getInput('category') || 'modulo';
+let readme_path = core.getInput('readme_path') || 'README.md';
+
+async function copyReadmeTemplate(categoryReadme, path) {
+    switch(categoryReadme) {
+        case 'modulo': 
+            templateFiles += `${category}.md`
+            await fs.copyFile(templateFiles, path, () => {
+                console.log("Copied Successfully!");
+            });
+        break;
+        case 'practica': 
+    
+        break;
+        case 'proyecto': 
+        break;
+    }
+
+}
+
+if(fs.existsSync(readme_path)) {
+    console.log("El fichero README.md existe");
+}
+else {
+    console.log("Creando fichero README.md...");
+    shell.exec(`touch README.md`);
+    readme_path = 'README.md';
+}
+
+copyReadmeTemplate(category, readme_path)
